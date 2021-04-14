6fa621d62a81bb71d9b7cdf079b86a00e23c7f7e
Mehdi Rahman <mehdi.rahman@mehdirahma-ltm1.internal.salesforce.com>
2021-04-14 23:59:47 +0100

    2021 timetable updates

--- js/RamadanTimetable.js
+++ js/RamadanTimetable.js
@@ -1,27 +1,99 @@
 ﻿var jsonTimetable = [
     {   
+        time : "04/15/2020 04:22:00", 
+        fasting : false
+    },
+    {   
+        time : "04/15/2020 20:01:00", 
+        fasting : true
+    },
+    {   
+        time : "04/16/2020 04:19:00", 
+        fasting : false
+    },
+    {   
+        time : "04/16/2020 20:03:00", 
+        fasting : true
+    },
+    {   
+        time : "04/17/2020 04:17:00", 
+        fasting : false
+    },
+    {   
+        time : "04/17/2020 20:04:00", 
+        fasting : true
+    },
+    {   
+        time : "04/18/2020 04:15:00", 
+        fasting : false
+    },
+    {   
+        time : "04/18/2020 20:06:00", 
+        fasting : true
+    },
+    {   
+        time : "04/19/2020 04:12:00", 
+        fasting : false
+    },
+    {   
+        time : "04/19/2020 20:08:00", 
+        fasting : true
+    },
+    {   
+        time : "04/20/2020 04:10:00", 
+        fasting : false
+    },
+    {   
+        time : "04/20/2020 20:10:00", 
+        fasting : true
+    },
+    {   
+        time : "04/21/2020 04:07:00", 
+        fasting : false
+    },
+    {   
+        time : "04/21/2020 20:11:00", 
+        fasting : true
+    },
+    {   
+        time : "04/22/2020 04:05:00", 
+        fasting : false
+    },
+    {   
+        time : "04/22/2020 20:13:00", 
+        fasting : true
+    },
+    {   
+        time : "04/23/2020 04:03:00", 
+        fasting : false
+    },
+    {   
+        time : "04/23/2020 20:15:00", 
+        fasting : true
+    },
+    {   
         time : "04/24/2020 04:00:00", 
         fasting : false
     },
     {   
-        time : "04/24/2020 20:17:00", 
+        time : "04/24/2020 20:16:00", 
         fasting : true
     },
     {   
-        time : "04/25/2020 03:57:00", 
+        time : "04/25/2020 03:58:00", 
         fasting : false
     },
     {   
-        time : "04/25/2020 20:19:00", 
+        time : "04/25/2020 20:18:00", 
         fasting : true
     },
     {   
-        time : "04/26/2020 03:55:00", 
+        time : "04/26/2020 03:56:00", 
         fasting : false
     },
     {   
         time : "04/26/2020 20:20:00", 
-        fasting : true
+        fasting: true
     },
     {   
         time : "04/27/2020 03:53:00", 
@@ -29,19 +101,19 @@
     },
     {   
         time : "04/27/2020 20:22:00", 
-        fasting : true
+        fasting: true
     },
     {   
-        time : "04/28/2020 03:50:00", 
+        time : "04/28/2020 03:51:00", 
         fasting : false
     },
     {   
-        time : "04/28/2020 20:24:00", 
+        time : "04/28/2020 20:23:00", 
         fasting : true
     },
     {   
-        time : "04/29/2020 03:48:00", 
-        fasting : false
+        time : "04/29/2020 03:49:00", 
+        fasting: false
     },
     {   
         time : "04/29/2020 20:25:00", 
@@ -49,7 +121,7 @@
     },
     {   
         time : "04/30/2020 03:46:00", 
-        fasting : false
+        fasting: false
     },
     {   
         time : "04/30/2020 20:27:00", 
@@ -57,58 +129,58 @@
     },
     {   
         time : "05/01/2020 03:44:00", 
-        fasting : false
+        fasting: false
     },
     {   
-        time : "05/01/2020 20:29:00", 
+        time : "05/01/2020 20:28:00", 
         fasting : true
     },
     {   
         time : "05/02/2020 03:42:00", 
-        fasting : false
+        fasting: false
     },
     {   
         time : "05/02/2020 20:30:00", 
         fasting : true
     },
     {   
-        time : "05/03/2020 03:39:00", 
-        fasting : false
+        time : "05/03/2020 03:40:00", 
+        fasting: false
     },
     {   
         time : "05/03/2020 20:32:00", 
         fasting : true
     },
     {   
-        time : "05/04/2020 03:37:00", 
-        fasting : false
+        time : "05/04/2020 03:38:00", 
+        fasting: false
     },
     {   
-        time : "05/04/2020 20:34:00", 
+        time : "05/04/2020 20:33:00", 
         fasting : true
     },
     {   
-        time : "05/05/2020 03:35:00", 
-        fasting : false
+        time : "05/05/2020 03:36:00", 
+        fasting: false
     },
     {   
         time : "05/05/2020 20:35:00", 
-        fasting: true
+        fasting : true
     },
     {   
-        time : "05/06/2020 03:33:00", 
-        fasting : false
+        time : "05/06/2020 03:34:00", 
+        fasting: false
     },
     {   
         time : "05/06/2020 20:37:00", 
-        fasting: true
+        fasting : true
     },
     {   
         time : "05/07/2020 03:31:00", 
-        fasting : false
+        fasting: false
     },
     {   
-        time : "05/07/2020 20:39:00", 
+        time : "05/07/2020 20:38:00", 
         fasting : true
     },
     {   
@@ -132,7 +204,7 @@
         fasting: false
     },
     {   
-        time : "05/10/2020 20:44:00", 
+        time : "05/10/2020 20:43:00", 
         fasting : true
     },
     {   
@@ -148,7 +220,7 @@
         fasting: false
     },
     {   
-        time : "05/12/2020 20:47:00", 
+        time : "05/12/2020 20:46:00", 
         fasting : true
     },
     {   
@@ -157,86 +229,6 @@
     },
     {   
         time : "05/13/2020 20:48:00", 
-        fasting : true
-    },
-    {   
-        time : "05/14/2020 03:17:00", 
-        fasting: false
-    },
-    {   
-        time : "05/14/2020 20:50:00", 
-        fasting : true
-    },
-    {   
-        time : "05/15/2020 03:15:00", 
-        fasting: false
-    },
-    {   
-        time : "05/15/2020 20:52:00", 
-        fasting : true
-    },
-    {   
-        time : "05/16/2020 03:13:00", 
-        fasting: false
-    },
-    {   
-        time : "05/16/2020 20:53:00", 
-        fasting : true
-    },
-    {   
-        time : "05/17/2020 03:12:00", 
-        fasting: false
-    },
-    {   
-        time : "05/17/2020 20:55:00", 
-        fasting : true
-    },
-    {   
-        time : "05/18/2020 03:10:00", 
-        fasting: false
-    },
-    {   
-        time : "05/18/2020 20:56:00", 
-        fasting : true
-    },
-    {   
-        time : "05/19/2020 03:08:00", 
-        fasting: false
-    },
-    {   
-        time : "05/19/2020 20:58:00", 
-        fasting : true
-    },
-    {   
-        time : "05/20/2020 03:07:00", 
-        fasting: false
-    },
-    {   
-        time : "05/20/2020 20:59:00", 
-        fasting : true
-    },
-    {   
-        time : "05/21/2020 03:05:00", 
-        fasting: false
-    },
-    {   
-        time : "05/21/2020 21:00:00", 
-        fasting : true
-    },
-    {   
-        time : "05/22/2020 03:03:00", 
-        fasting: false
-    },
-    {   
-        time : "05/22/2020 21:02:00", 
-        fasting : true
-    },
-    {   
-        time : "05/23/2020 03:02:00", 
-        fasting: false
-    },
-    {   
-        time : "05/23/2020 21:03:00", 
         fasting : true
     }
 ]
